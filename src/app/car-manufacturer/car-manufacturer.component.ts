ba788250dd50be86e3930946bc045139d13d55b9
Chandradas-Dessai <37234889+Chandradas-Dessai@users.noreply.github.com>
2019-09-11 23:09:50 +0530

    reverted suscibe changes

--- src/app/car-manufacturer/car-manufacturer.component.ts
+++ src/app/car-manufacturer/car-manufacturer.component.ts
@@ -62,31 +62,21 @@ export class CarManufacturerComponent implements OnInit {
   }
 
  
-//   deleteManufacturer(mId: any){
-//     const id  = `${mId}`;
-//   if(confirm(`Are you sure you want to delete the Manufacturer with id ${mId}`)){
-//   this.carManufacturerService.deleteManufacturerById(id).subscribe(response => {
-//     if(response.status=="success"){
-//       this.toastr.success(response.message, 'Success', {timeOut: 5000});
-//       this.listAllManufacturers();
-//       this.manufacturerForm.reset();
-//     }else{
-//       this.toastr.error('Unable to delete Manufacturer!', 'Error', {timeOut: 5000})
-//     }
-//     });
-//   }
-// }
+  deleteManufacturer(mId: any){
+    const id  = `${mId}`;
+  if(confirm(`Are you sure you want to delete the Manufacturer with id ${mId}`)){
+  this.carManufacturerService.deleteManufacturerById(id).subscribe(response => {
+    if(response.status=="success"){
+      this.toastr.success(response.message, 'Success', {timeOut: 5000});
+      this.listAllManufacturers();
+      this.manufacturerForm.reset();
+    }else{
+      this.toastr.error('Unable to delete Manufacturer!', 'Error', {timeOut: 5000})
+    }
+    });
+  }
+}
 
-deleteManufacturer(mId: any){
-  const id  = `${mId}`;
-if(confirm(`Are you sure you want to delete the Manufacturer with id ${mId}`)){
-this.carManufacturerService.deleteManufacturerById(id).subscribe( 
-  () => {
-  this.listAllManufacturers();
-//       this.manufacturerForm.reset();
-});
-}
-}
 
 }
 
